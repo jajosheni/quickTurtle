e059c7b49fa6d8ab8f10450f687129425cc14ce8
Sheni Hamitaj <shen.i@live.com>
2019-01-22 19:09:12 +0300

    Update quickTurtle.sh

--- bash/quickTurtle.sh
+++ bash/quickTurtle.sh
@@ -10,12 +10,12 @@ AVGLENGTH=$TOTLENGTH/$THREADS
 DOWNRANGE=0
 UPRANGE=$((DOWNRANGE+AVGLENGTH))
 
-for i in {1..16}; do
+for i in {1..$THREADS}; do
 echo '#!/bin/bash' > "$i.sh";
 echo "URL='$URL'" >> "$i.sh";
 
-if (($i == 16)); then echo "curl -s -r $DOWNRANGE- \$URL -o part$i" >> "$i.sh";
-elif (($i !=16)); then echo "curl -s -r $DOWNRANGE-$UPRANGE \$URL -o part$i" >> "$i.sh"; fi
+if (($i == $THREADS)); then echo "curl -s -r $DOWNRANGE- \$URL -o part$i" >> "$i.sh";
+elif (($i != $THREADS)); then echo "curl -s -r $DOWNRANGE-$UPRANGE \$URL -o part$i" >> "$i.sh"; fi
 
 DOWNRANGE=$((UPRANGE+1));
 UPRANGE=$((UPRANGE+AVGLENGTH));
